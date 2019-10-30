04c8d12dd9ac6c54384032e5eaf8ffb63d615168
Peter Cuevas Santana <petercsantana@live.com>
2019-10-30 02:49:54 -0300

    Connection ok

---
+++ TradingManager/TradingManager.Data.Context/Interface/ITradingManagerContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Data;
+
+namespace TradingManager.Data.Context.Interface
+{
+  interface ITradingManagerContext : IDisposable
+  {
+    IDbConnection Connection { get; }
+  }
+}
---
+++ TradingManager/TradingManager.Data.Context/TradingManagerContext.cs
@@ -0,0 +1,42 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+using TradingManager.Data.Context.Interface;
+
+namespace TradingManager.Data.Context
+{
+  public class TradingManagerContext : ITradingManagerContext
+  {
+    private readonly string _connectionString;
+    private IDbConnection _connection;
+
+    public TradingManagerContext(string connectionString)
+    {
+      _connectionString = connectionString;
+    }
+
+    public IDbConnection Connection
+    {
+      get
+      {
+        if (_connection == null)
+        {
+          _connection = new MySqlConnection(_connectionString);
+
+        }
+        if (_connection.State != ConnectionState.Open)
+        {
+          _connection.Open();
+        }
+        return _connection;
+      }
+    }
+
+    public void Dispose()
+    {
+      if (_connection != null && _connection.State == ConnectionState.Open)
+      {
+        _connection.Close();
+      }
+    }
+  }
+}
